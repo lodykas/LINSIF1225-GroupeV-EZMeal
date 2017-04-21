ec137e133abfcc429b6d96ddddee981ed1e5890a
lodycas <lodycas@gmail.com>
2017-04-21 13:20:13 +0200

    first commit creation de profil

---
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/CreateProfile.java
@@ -0,0 +1,16 @@
+package com.lsinf1225.ezmeal.ezmeal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lodycas on 21-04-17.
+ */
+
+public class CreateProfile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createprofile);
+    }
+}
