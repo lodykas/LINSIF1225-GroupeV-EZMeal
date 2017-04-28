aba3205418f0ffb92bd19f7e0ebfa4226ef638a1
Maxime2702 <maxime.benere@student.uclouvain.be>
2017-04-28 12:00:24 +0200

    test commit

---
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/ModifyProfileActivity.java
@@ -0,0 +1,13 @@
+package com.lsinf1225.ezmeal.ezmeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profile);
+    }
+}
