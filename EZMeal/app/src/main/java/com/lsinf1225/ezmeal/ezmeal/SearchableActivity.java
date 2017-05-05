9116eac12c4e098e48df21db68fef5830ec9efd8
lodykas <lucas.ody@student.uclouvain.be>
2017-05-05 09:57:17 +0200

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master' into recherche"
    This reverts commit 9b1ab7513268cf3edb9625fa8d06dc7dec8382e5, reversing
    changes made to 84713d3c08249cab9b02d1c0312d17d25e54c7e2.
    
    merge dans le mauvais sens

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SearchableActivity.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SearchableActivity.java
@@ -6,12 +6,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 /**
  * Created by Lodycas on 04-05-17.
@@ -29,27 +27,21 @@ public class SearchableActivity extends AppCompatActivity{
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
 
-
-
-            ListView l = (ListView) findViewById(R.id.listsearch);
-            SQLiteManager db = new SQLiteManager(getApplicationContext());
-
-            CursorAdapter c = new CursorAdapter(getApplicationContext(),db.research(query)) {
+            CursorAdapter c = new CursorAdapter() {
                 @Override
                 public View newView(Context context, Cursor cursor, ViewGroup parent) {
-                    return LayoutInflater.from(context).inflate(R.layout.search_item, parent, false);
+                    return null;
                 }
 
                 @Override
                 public void bindView(View view, Context context, Cursor cursor) {
-                    // Find fields to populate in inflated template
-                    TextView recipe = (TextView) view.findViewById(R.id.searchitem_name);
-                    // Extract properties from cursor
-                    String name = cursor.getString(cursor.getColumnIndexOrThrow("NomRecette"));
-                    // Populate fields with extracted properties
-                    recipe.setText(name);
+                    super.bindView(view,context,cursor);
                 }
             };
+
+            ListView l = findViewById(R.id.listsearch);
+            SQLiteManager db = new SQLiteManager(getApplicationContext());
+            c.bindView(l,getApplicationContext(),db.research(query));
         }
     }
 }
