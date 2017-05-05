19ac46b5b2039132dcb3b375f3dcc96984648ad5
Maxime2702 <maxime.benere@student.uclouvain.be>
2017-05-05 12:02:43 +0200

    Confirm Password lors de creation

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/CreateProfileActivity.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/CreateProfileActivity.java
@@ -33,14 +33,17 @@ public class CreateProfileActivity extends AppCompatActivity {
                 //I get all the data the user entered
                 String usermail = ((EditText) findViewById(R.id.usermailedit)).getText().toString();
                 String password = ((EditText) findViewById(R.id.passwordedit)).getText().toString();
+                String password2 = ((EditText) findViewById(R.id.passwordedit2)).getText().toString();
                 RadioGroup sex = (RadioGroup) findViewById(R.id.sexSelect);
                 RadioButton sexB = (RadioButton) findViewById(sex.getCheckedRadioButtonId());
                 String birth = ((EditText) findViewById(R.id.birthedit)).getText().toString();
                 String country = ((EditText) findViewById(R.id.countryedit)).getText().toString();
                 String oriCountry = ((EditText) findViewById(R.id.origincountryedit)).getText().toString();
 
-                if (usermail == " " || password == " " || birth == " " || country == " " || oriCountry == " " || sexB == null){
+                if (usermail == " " || password == " " || password2 == " " || birth == " " || country == " " || oriCountry == " " || sexB == null){
                     Toast.makeText(getApplicationContext(), "All fields are mandatory!", 3000).show();
+                } else if (!(password.equals(password2))) {
+                    Toast.makeText(getApplicationContext(), "Password not match !", 3000).show();
                 }
                 else {//if everything's complete, I create the object and update the database
                     User user = new User(usermail, sexB.getText().charAt(0), password, birth, country, oriCountry);
