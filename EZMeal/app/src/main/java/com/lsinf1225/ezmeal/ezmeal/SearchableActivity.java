1aa5c4e3f5f0e9ce7947a8ad31079f038a6cd669
lodykas <lucas.ody@student.uclouvain.be>
2017-05-05 09:47:47 +0200

    En théorie fonctionne, faut mettre des recettes dans la BDD pour tester

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SearchableActivity.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SearchableActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Lodycas on 04-05-17.
@@ -27,21 +29,27 @@ public class SearchableActivity extends AppCompatActivity{
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
 
-            CursorAdapter c = new CursorAdapter() {
+
+
+            ListView l = (ListView) findViewById(R.id.listsearch);
+            SQLiteManager db = new SQLiteManager(getApplicationContext());
+
+            CursorAdapter c = new CursorAdapter(getApplicationContext(),db.research(query)) {
                 @Override
                 public View newView(Context context, Cursor cursor, ViewGroup parent) {
-                    return null;
+                    return LayoutInflater.from(context).inflate(R.layout.search_item, parent, false);
                 }
 
                 @Override
                 public void bindView(View view, Context context, Cursor cursor) {
-                    super.bindView(view,context,cursor);
+                    // Find fields to populate in inflated template
+                    TextView recipe = (TextView) view.findViewById(R.id.searchitem_name);
+                    // Extract properties from cursor
+                    String name = cursor.getString(cursor.getColumnIndexOrThrow("NomRecette"));
+                    // Populate fields with extracted properties
+                    recipe.setText(name);
                 }
             };
-
-            ListView l = findViewById(R.id.listsearch);
-            SQLiteManager db = new SQLiteManager(getApplicationContext());
-            c.bindView(l,getApplicationContext(),db.research(query));
         }
     }
 }
