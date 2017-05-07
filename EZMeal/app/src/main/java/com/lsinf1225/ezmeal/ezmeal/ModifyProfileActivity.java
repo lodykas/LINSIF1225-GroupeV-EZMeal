f6283015c872a0532c9bef6c1b0b2dd5ff6234a1
patmarie <pat.haza@student.uclouvain.be>
2017-05-07 17:26:23 +0200

    spinner dans modify profile

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/ModifyProfileActivity.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/ModifyProfileActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ModifyProfileActivity extends AppCompatActivity {
@@ -15,6 +17,11 @@ public class ModifyProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_profile);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         Button b = (Button) findViewById(R.id.button201);
 
         b.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +29,9 @@ public class ModifyProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String password = ((EditText) findViewById(R.id.passwordedit2)).getText().toString();
                 String password2 = ((EditText) findViewById(R.id.passwordedit)).getText().toString();
-                String country = ((EditText) findViewById(R.id.countryedit2)).getText().toString();
+                Spinner spinner1 = (Spinner)findViewById(R.id.spinner1);
+                String country = spinner1.getSelectedItem().toString();
+                //String country = ((EditText) findViewById(R.id.countryedit2)).getText().toString();
                 if (password.equals(password2)) {
                     SQLiteManager db = new SQLiteManager(getApplicationContext());
                     db.updateUser(getIntent().getStringExtra("usermail"), password, country);
