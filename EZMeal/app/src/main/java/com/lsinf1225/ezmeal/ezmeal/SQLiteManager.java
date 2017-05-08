402029332c920bdc239d62887a5b4121db7b942b
patmarie <pat.haza@student.uclouvain.be>
2017-05-08 22:18:09 +0200

    bdd suite

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SQLiteManager.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/SQLiteManager.java
@@ -98,6 +98,58 @@ public class SQLiteManager extends SQLiteOpenHelper {
         d.bindString(1,"mojito");
         d.bindString(2,"beverage");
         d.execute();
+        d.bindString(1,"soupe à l'oignon");
+        d.bindString(2,"entree");
+        d.execute();
+        d.bindString(1,"macaroni au fromage");
+        d.bindString(2,"plat");
+        d.execute();
+        d.bindString(1,"spahetti bolognaise");
+        d.bindString(2,"plat");
+        d.execute();
+        d.bindString(1,"salade césar");
+        d.bindString(2,"entree");
+        d.execute();
+        d.bindString(1,"salade césar");
+        d.bindString(2,"plat");
+        d.execute();
+        d.bindString(1,"mousse au chocolat");
+        d.bindString(2,"dessert");
+        d.execute();
+        d.bindString(1,"rhum citron miel");
+        d.bindString(2,"beverage");
+        d.execute();
+        d.bindString(1,"gaufre belge");
+        d.bindString(2,"dessert");
+        d.execute();
+        d.bindString(1,"chocolat chaud");
+        d.bindString(2,"beverage");
+        d.execute();
+        d.bindString(1,"curry japonais");
+        d.bindString(2,"plat");
+        d.execute();
+        d.bindString(1,"donut new yorkais");
+        d.bindString(2,"dessert");
+        d.execute();
+        d.bindString(1,"gin tonic à la framboise");
+        d.bindString(2,"beverage");
+        d.execute();
+        d.bindString(1,"granola maison");
+        d.bindString(2,"dessert");
+        d.execute();
+        d.bindString(1,"thai glacé thaï");
+        d.bindString(2,"beverage");
+        d.execute();
+        d.bindString(1,"crepe sucree");
+        d.bindString(2,"dessert");
+        d.execute();
+        d.bindString(1,"mojito");
+        d.bindString(2,"beverage");
+        d.execute();
+        d.bindString(1,"boisson saine et sans allergènes");
+        d.bindString(2,"bevrage");
+        d.execute();
+
 
         db.execSQL("CREATE TABLE IF NOT EXISTS \"Sous_catégorie\" (\"NomRecette\" NOT NULL,\"Sous_catégorie\" NOT NULL);");
 
@@ -110,7 +162,7 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 "quelques minutes avant la fin de la cuisson, ajouter le porto (optionnel), mélanger bien et servir dans des bols individuels.\n" +
                 "placer les bols côte à côte sur une grille allant au four, couvrir le centre de chaque bol avec un morceau de pain frit, casser au milieu du pain un oeuf sans le blanc, saupoudrer avec le parmesan et hop, au grill maximum pendant quelques moments, histoire de faire prendre un peu de couleur à l'appareil.\n" +
                 "bon appétit.");
-        r.bindString(4,"05/05/17");
+        r.bindString(4,"01/04/17");
         r.bindString(5,"occidentale");
         r.execute();
         r.bindString(1,"macaroni au fromage");
@@ -119,7 +171,7 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 "remettez à feu doux jusqu'à ce que le mélange soit lisse. poivrez et relevez de muscade. \n" +
                 "pendant ce temps, faîtes cuire les pâtes suivant le temps indiqué sur votre paquet (pâtes fraîches c'est encore meilleur). \n" +
                 "egouttez les pâtes, versez-les dans la casserole contenant la sauce aux trois fromages. mélangez rapidement, parsemez du reste de parmesan et servez.");
-        r.bindString(4,"05/05/17");
+        r.bindString(4,"02/04/17");
         r.bindString(5,"italienne");
         r.execute();
         r.bindString(1,"spaghetti bolognaise");
@@ -135,7 +187,7 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 "baisser ensuite le feu et laissez mijoter à couvert 1h à 1h30, de façon à ce que le vin s'évapore. \n" +
                 "\n" +
                 "faites cuire les spaghetti, puis mettez-les dans un plat. ajoutez la sauce bolognaise.");
-        r.bindString(4,"06/05/17");
+        r.bindString(4,"03/04/17");
         r.bindString(5,"italienne");
         r.execute();
         r.bindString(1,"salade césar");
@@ -149,28 +201,42 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 "mettez la salade dans un saladier et versez dessus la sauce. mélangez bien le tout et laissez de côté pendant une dizaine de minutes afin que la salade s'imprègne bien de la sauce. \n" +
                 "coupez les oeufs en 4, dans le sens de la longueur. mélangez-les à la salade afin que le jaune se mêle à la sauce, puis ajoutez le parmesan râpé et les croûtons. \n" +
                 "servez aussitôt.");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"occidentale");
+        r.bindString(4,"04/04/17");
+        r.bindString(5,"grecque");
         r.execute();
         r.bindString(1,"mousse au chocolat");
         r.bindString(2,"moussechocolat");
-        r.bindString(3,"battre les blancs d'oeufs en neige, incorporez-y le mélange du chocolat, beurre et jaune d'oeuf.");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"occidentale");
+        r.bindString(3,"séparer les blancs des jaunes d'oeufs.\n" +
+                "\n" +
+                "faire ramollir le chocolat dans une casserole au bain-marie. \n" +
+                "\n" +
+                "hors du feu, incorporer les jaunes et le sucre. \n" +
+                "\n" +
+                "battre les blancs en neige ferme et les ajouter délicatement au mélange à l'aide d'une spatule. \n" +
+                "\n" +
+                "verser dans une terrine ou des verrines et mettre au frais 1 heure ou 2 minimum.\n");
+        r.bindString(4,"05/04/17");
+        r.bindString(5,"belge");
         r.execute();
         r.bindString(1,"milshake au fruit rouges");
         r.bindString(2,"berryshake");
         r.bindString(3,"versez le lait d'amande dans le blender, rajoutez le mélange de fruits rouges congelés, versez un dilet de sirop d'agave et mixez le tout. servir dans un grand verre et parsemez de fruits rouges frais");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"us");
+        r.bindString(4,"06/04/17");
+        r.bindString(5,"espagnole");
         r.execute();
-        r.bindString(1,"Rhum citon miel");
+        r.bindString(1,"rhum citron miel");
         r.bindString(2,"alcoolcitron");
-        r.bindString(3,"dans un verre à alcool, ajoutez 3 c.à.s de rhum ainsi que de miel, remplisez le reste du verre en thé de citron froid. servez avec des tranches de citron");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"tex-mex");
+        r.bindString(3,"composer chacun des verres de la manière suivante :\n" +
+                "verser 2 cl de Rhum, saupoudrer d'une pincée de cannelle et bien mélanger pour éviter les grumeaux.\n" +
+                "Couper les 2 feuilles de menthe en fines lanières et les écraser un peu dans le rhum avec un pilon.\n" +
+                "passez la boisson au tamis pour retirer les morceaux de menthe.\n" +
+                "couper une rondelle de citron de 5 mm d'épaisseur et la presser au-dessus du verre.\n" +
+                "vous pouvez ensuite mettre la rondelle dans le verre pour la presentation.\n" +
+                "ajouter 2 glaçons et bien remuer.");
+        r.bindString(4,"07/04/17");
+        r.bindString(5,"mexicaine");
         r.execute();
-        r.bindString(1,"gauffre belge");
+        r.bindString(1,"gaufre belge");
         r.bindString(2,"belgianwaffle");
         r.bindString(3,"tamiser la farine.\n" +
                 "séparer les jaunes et les blancs des oeufs en ne gardant que 4 blancs.\n" +
@@ -184,62 +250,94 @@ public class SQLiteManager extends SQLiteOpenHelper {
                 "cuire les gaufres dans un fer bien chaud.\n" +
                 "dès que la ou les portions de pâte sont versées, il faut fermer le gaufrier et le renverser de suite pour avoir des gaufres régulières et gonflées.\n" +
                 "aussitôt que la gaufre est dorée sur les deux faces, la sortir du fer, la saupoudrer de sucre glace et servir très chaud.");
-        r.bindString(4,"08/05/17");
+        r.bindString(4,"08/04/17");
         r.bindString(5,"belge");
         r.execute();
         r.bindString(1,"chocolat chaud de noel");
         r.bindString(2,"chocochaud");
         r.bindString(3,"dans une casserole chauffez un verre de lait en surveillant la cuisson du lait, lorsque le lait frémit doucement ajoutez y 4 barres de choclat noir concassé, mélangez bien et rajoutez à la préparation un baton de canelle, 2 clous de girofle ainsi qu'une c.à.c de muscade, sucrez à votre convenance. servez avec de la chantilly saupoudrée au cacao ");
-        r.bindString(4,"06/05/17");
+        r.bindString(4,"09/04/17");
         r.bindString(5,"russie");
         r.execute();
         r.bindString(1,"curry japonais");
         r.bindString(2,"curryjapan");
-        r.bindString(3,"blabla");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"asie");
+        r.bindString(3,"faire revenir les morceaux de porc dans un peu d'huile jusqu'à ce qu'ils soient bien grillés. Les retirer et faire un roux avec la graisse qu'il y a dans la casserole (en rajouter si nécessaire). ajouter de l'eau (un litre à un litre et demi), ainsi que le curry. Faire bouillir. ajouter les morceaux de viande, les carottes et les navets. après 10 minutes de cuisson ajouter les pommes de terre et les courgettes et laisser cuire sur feu doux encore 20 à 25 minutes.\n" +
+                "\n" +
+                "le plat doit ressembler à une soupe que l'on servira avec un bol de riz blanc. saler à convenance. \n" +
+                "on peut mettre aussi d'autres légumes comme haricots verts, poivrons…\n");
+        r.bindString(4,"010/04/17");
+        r.bindString(5,"japonaise");
         r.execute();
         r.bindString(1,"donut new yorkais");
         r.bindString(2,"donut");
-        r.bindString(3,"blabla");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"us");
+        r.bindString(3,"mélanger la farine, la levure, le sel, la cannelle et la muscade. \n" +
+                "dans un autre bol, mélanger les oeufs (battus), le sucre et la vanille. mélanger jusqu'à ce que le mélange soit épais.\n" +
+                "mélanger le lait et le beurre fondu, y ajouter le mélange de farine et battre jusqu'à ce que le mélange soit homogène.\n" +
+                "couvrir et mettre au réfrigérateur pendant 2 heures.\n" +
+                "sur une planche farinée, étaler la pâte sur une épaisseur de 1 cm environ. découper les donuts à l'aide d'un bol ou autre.\n" +
+                "mettre à frire pendant une minute ou jusqu'à ce soit légerement doré puis retourner a l'aide d'un écumoire de l'autre coté et laisser cuire à nouveau une minute.\n" +
+                "retirer de la friteuse et poser sur un papier absorbant.garnissez avec du sucre, du choclat fondu...");
+        r.bindString(4,"11/04/17");
+        r.bindString(5,"américaine");
         r.execute();
         r.bindString(1,"gin tonic à la framboise");
         r.bindString(2,"ginframboise");
-        r.bindString(3,"blabla");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"espagne");
+        r.bindString(3,"\n" +
+                "Mélanger tous les ingrédients dans un shaker.\n" +
+                "Ajouter des glaçons.\n" +
+                "Agiter vigoureusement 10 secondes.\n" +
+                "Servir dans un verre avec des framboises fraîches. \n" +
+                "C'est prêt! Bonne dégustation.");
+        r.bindString(4,"12/04/17");
+        r.bindString(5,"anglaise");
         r.execute();
         r.bindString(1,"granola maison");
         r.bindString(2,"granola");
-        r.bindString(3,"dans un bol melangez le musli avec du miel, etalez ensuite sur une plaque et enfournez 30 min à 180 degré, sortez la plaque et mélangez le tout, ajoutez des nox, noisettes et raison sec");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"suède");
+        r.bindString(3,"préchauffer le four à 150°C.\n" +
+                "mélanger l'huile, le miel, le sirop d'érable et l'arôme de vanille au fond d'un grand saladier.\n" +
+                "concasser les fruits secs à l'aide d'un grand couteau.\n" +
+                "mélanger les flocons d'avoine et les fruits sec dans le saladier.\n" +
+                "etaler le mélange sur une plaque de cuisson et enfourner pendant 15 minutes.\n" +
+                "pendant la cuisson, couper le chocolat à dessert en morceaux (à l'aide d'un couteau).\n" +
+                "laisser refroidir le granola avant d'y ajouter le chocolat.\n" +
+                "conserver dans un bocal hermétique.");
+        r.bindString(4,"13/04/17");
+        r.bindString(5,"suèdoise");
         r.execute();
         r.bindString(1,"thai glacé thaï");
         r.bindString(2,"icedtea");
-        r.bindString(3,"demandez à pat");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"thaïlande");
+        r.bindString(3,"porter l'eau à ébullition. Mettre le thé dans le filtre d'une théière. verser doucement l'eau sur le thé et laisser infuser jusqu'à ce que le thé soit très fort et prenne une couleur orange foncé.\n" +
+                "ajouter le lait concentré sucré au thé, remuer et laisser refroidir. ajouter des glaçons dans 4 verres long drink et verser le thé dans les verres de manière à les remplir à moitié. compléter avec le lait et servir immédiatement.\n");
+        r.bindString(4,"14/04/17");
+        r.bindString(5,"thaïlandaise");
         r.execute();
-        r.bindString(1,"macaron");
-        r.bindString(2,"macaron");
-        r.bindString(3,"paul vous servira");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"france");
+        r.bindString(1,"crepes sucree");
+        r.bindString(2,"crepes");
+        r.bindString(3,"\n" +
+                "verser 250 g de farine, casser 4 œufs et mélanger bien à la farine.\n" +
+                "ajouter petit à petit le lait tout en mélangeant bien pour éviter les grumeaux.\n" +
+                "ajouter la pincée de sel.\n" +
+                "ajouter les 3 sucres (vanillé, de canne et en poudre).\n" +
+                "verser le Cognac toujours en mélangeant bien.\n" +
+                "ajouter les 50 g de beurre fondu.\n" +
+                "faire chauffer la poêle à feu vif en y ajoutant un peu d'huile dont vous essuyez l'excédent avec un papier absorbant.\n" +
+                "faire des crêpes fines si possible.");
+        r.bindString(4,"15/04/17");
+        r.bindString(5,"française");
         r.execute();
         r.bindString(1,"mojito");
         r.bindString(2,"mojito");
-        r.bindString(3,"bjbzfz");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"cuba");
+        r.bindString(3,"ajouter le sucre en poudre dans le verre, puis le jus d'1/2 citron vert.\n" +
+                "ecraser 4 feuilles de menthe, ajouter le rhum et compléter avec l'eau gazeuse. \n" +
+                "remuer et ajouter 1 branche de menthe.");
+        r.bindString(4,"16/04/17");
+        r.bindString(5,"cubaine");
         r.execute();
         r.bindString(1,"boisson saine et sans allergènes");
         r.bindString(2,"pastequeboisson");
         r.bindString(3,"congelez des morceau de chair de pastèque, une fois les morceaux dur comme des glacons, versez un verre remplis de pastèque congelés avec un verre d'eau et une poignée de menthe. mixer le tout ");
-        r.bindString(4,"06/05/17");
-        r.bindString(5,"afrique");
+        r.bindString(4,"17/04/17");
+        r.bindString(5,"africaine");
         r.execute();
 
 
