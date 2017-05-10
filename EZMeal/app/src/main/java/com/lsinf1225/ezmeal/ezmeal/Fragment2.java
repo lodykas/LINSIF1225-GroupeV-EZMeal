b1ac9d932c8945a80cfc888bdbedd74a304fadd1
patmarie <pat.haza@student.uclouvain.be>
2017-05-10 15:16:17 +0200

    suite bdd

--- EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/Fragment2.java
+++ EZMeal/app/src/main/java/com/lsinf1225/ezmeal/ezmeal/Fragment2.java
@@ -2,6 +2,8 @@ package com.lsinf1225.ezmeal.ezmeal;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
